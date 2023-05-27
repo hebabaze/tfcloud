ce7e2e26a6b3bc826246ce45e4ddff3733bcdd79
hebabaze <54450458+hebabaze@users.noreply.github.com>
2023-05-27 20:51:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
 
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mrcxdftfnot-${terraform.workspace}"
+  tags = {
+    Name        = local.tag_value
+    Environment = "MIP"
+  }
+}
+
+resource "aws_s3_bucket_acl" "mybucket_acl" {  
+  bucket = aws_s3_bucket.mybucket.id  
+  acl = "private"
+}
