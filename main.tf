02b9236e267a42335861b28d466bf489be609926
hebabaze <54450458+hebabaze@users.noreply.github.com>
2023-05-27 21:28:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
-variable "tag" {
-  description = "Variable description"
-}
-locals {
-  tag_value = terraform.workspace == "default" ? "WladHadou" : "Sebt_El_Gurdane"
-}
-
 resource "aws_s3_bucket" "mybucket" {
   bucket = "mrcxdftfnot-${terraform.workspace}"
+
+
   tags = {
     Name        = local.tag_value
     Environment = "MIP"
   }
+
 }
 
-resource "aws_s3_bucket_acl" "mybucket_acl" {  
-  bucket = aws_s3_bucket.mybucket.id  
-  acl = "private"
+locals {
+  tag_value = terraform.workspace == "default" ? "WladHadou" : "Sebt_El_Gurdane"
 }
